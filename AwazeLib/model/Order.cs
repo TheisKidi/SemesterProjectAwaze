cc5c09502159813faedc4a9db81f249dd4f1c473
TheisKidi <theis.k@hotmail.com>
2023-04-28 11:12:49 +0200

    Order Class complete

--- AwazeLib/model/Order.cs
+++ AwazeLib/model/Order.cs
@@ -8,5 +8,29 @@ namespace AwazeLib.model
 {
     public class Order
     {
+        public int OrderId { get; set; }
+        public Guest Guest { get; set; }
+        public Property Property { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public bool IsBooked { get; set; }
+
+        public Order(int orderId, Guest guest, Property property, DateTime arrivalDate, DateTime departureDate, bool isBooked)
+        {
+            OrderId = orderId;
+            Guest = guest;
+            Property = property;
+            ArrivalDate = arrivalDate;
+            DepartureDate = departureDate;
+            IsBooked = isBooked;
+        }
+
+        public Order() : this(0, 0, new Property, DateTime.Now, DateTime.Now, false) { }
+
+        public override string ToString()
+        {
+            return $"Order Id: {OrderId}, Guest: {Guest}, Property: {Property}, Arrival Date: {ArrivalDate}, Departure Date: {DepartureDate}, Is Booked: {IsBooked}";
+        }
+
     }
 }
