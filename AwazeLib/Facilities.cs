db853590ed84d63bcd9ec1d85957133d18bc19c0
TheisKidi <theis.k@hotmail.com>
2023-04-28 10:56:40 +0200

    fff

--- AwazeLib/Facilities.cs
+++ AwazeLib/Facilities.cs
@@ -1,7 +1,38 @@
 ﻿namespace AwazeLib
 {
-    public class Class1
+
+    public enum HouseType {HolidayCottage, HollidayApartment, HouseBoat}
+    public class Facilities
     {
+        public int Persons { get; set; }
+        public int Bedrooms { get; set; }
+        public int Bathrooms { get; set; }
+        public bool Sustainable { get; set; }
+        public bool AllowPets { get; set; }
+        public bool Wifi { get; set; }
+        public int Tv { get; set; }
+        public HouseType Type { get; set;}
+
+        public Facilities(int persons, int bedrooms, int bathrooms, bool sustainable, bool allowPets, bool wifi, int tv, HouseType type)
+        {
+            Persons = persons;
+            Bedrooms = bedrooms;
+            Bathrooms = bathrooms;
+            Sustainable = sustainable;
+            AllowPets = allowPets;
+            Wifi = wifi;
+            Tv = tv;
+            Type = type;
+        }   
+
+        public Facilities() : this(0, 0, 0, false, false, false, false, HouseType.HolidayCottage) 
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"Perons: {Persons}, Bedrooms: {Bedrooms}, Bathrooms: {Bathrooms}, Sustainable: {Sustainable}, Is pets allowed: {AllowPets}, Wifi: {Wifi}, Tv: {Tv}, HouseType: {HouseType}";
+        }
 
     }
 }
