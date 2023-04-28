624b415b87d1d9f5af47792a09b0d727005f6f39
TheisKidi <theis.k@hotmail.com>
2023-04-28 11:23:31 +0200

    Guest Class added

--- AwazeLib/model/Guest.cs
+++ AwazeLib/model/Guest.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace AwazeLib.model
 {
-    public class Guest
+    public class Guest : Profile
     {
+        public string MyBookingId { get; set; }
+
+
+        public Guest (string surname, string lastname, string email, string phone, bool isOwner, string password, string myBookingId)
+            : base(surname, lastname, email, phone, isOwner, password)
+        {
+            MyBookingId = myBookingId;
+        }
+
+        public Guest() : this ("Default surname", "Default lastname", "Default email", "Default phone", false, "Default Password", "DefaultBookingId" )
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"Surname: {SurName}, LastName: {LastName}, Email: {Email}, Phone: {Phone}, IsOwner: {IsOwner}, " +
+                   $"Password: {Password}, MyBookingId: {MyBookingId}";
+        }
+
+
     }
 }
