c64cb73cdd8053827d023c0876503ba6e204b9ac
BigHars <hassethore@gmail.com>
2023-05-01 12:31:54 +0200

    DeletePropery and EditProperty done V1

--- SemesterProjectAwaze/Pages/Sites/DeleteProperty.cshtml.cs
+++ SemesterProjectAwaze/Pages/Sites/DeleteProperty.cshtml.cs
@@ -1,12 +1,35 @@
+using AwazeLib.model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SemesterProjectAwaze.Services;
 
 namespace SemesterProjectAwaze.Pages.Sites
 {
     public class DeletePropertyModel : PageModel
     {
-        public void OnGet()
+        private IGenericRepositoryService<Property> _propertyService;
+
+        public DeletePropertyModel(IGenericRepositoryService<Property> service)
         {
+            _propertyService = service;
+        }
+
+        public Property Property { get; set; }
+
+        public void OnGet(string id)
+        {
+            Property = _propertyService.GetById(id);
+        }
+
+        public IActionResult OnPostDelete(string id)
+        {
+            _propertyService.Delete(id);
+            return RedirectToPage("Index");
+        }
+
+        public IActionResult OnPostCancel()
+        {
+            return RedirectToPage("Index");
         }
     }
 }
