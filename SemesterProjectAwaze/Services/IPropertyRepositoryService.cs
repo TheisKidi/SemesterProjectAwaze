7501a714e9445c50b5bdb8259844f5fb0169d9cc
BigHars <hassethore@gmail.com>
2023-05-01 10:09:42 +0200

    PropertyRepository update V1

--- SemesterProjectAwaze/Services/IPropertyRepositoryService.cs
+++ SemesterProjectAwaze/Services/IPropertyRepositoryService.cs
@@ -1,11 +1,12 @@
 ﻿using AwazeLib.model;
+using System.Data.SqlClient;
 
 namespace SemesterProjectAwaze.Services
 {
     public interface IPropertyRepositoryService
     {
         public List<Property> GetAll();
-        public Property GetById(int id);
+        public Property GetById(string id);
         public Property Create(Property property);
         public Property Delete(string id);
         public Property Update(string id, Property property);
