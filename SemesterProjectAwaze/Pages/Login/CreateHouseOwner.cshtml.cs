d6eb548ef37e29f1200a279a60cc41d8471f3eb4
TheisKidi <theis.k@hotmail.com>
2023-05-04 09:56:05 +0200

    surname = firstname

--- SemesterProjectAwaze/Pages/Login/CreateHouseOwner.cshtml.cs
+++ SemesterProjectAwaze/Pages/Login/CreateHouseOwner.cshtml.cs
@@ -1,12 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SemesterProjectAwaze.Services;
 
 namespace SemesterProjectAwaze.Pages.Login
 {
+    [BindProperties]
     public class CreateHouseOwnerModel : PageModel
     {
+
+        private IGenericRepositoryService<HouseOwnerRepositoryService> _repo;
+
+        public CreateHouseOwnerModel(IGenericRepositoryService<HouseOwnerRepositoryService> repo)
+        {
+            _repo = repo;
+        }
+
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+
+
         public void OnGet()
         {
+
         }
+
+
+
     }
 }
