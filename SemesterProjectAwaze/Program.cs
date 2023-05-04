b77d1a35fbe3128664845c23e88a7e45c49aacac
BigHars <hassethore@gmail.com>
2023-05-04 10:44:42 +0200

    Added ILoginService Singleton

--- SemesterProjectAwaze/Program.cs
+++ SemesterProjectAwaze/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddSingleton<IGenericRepositoryService<Property>, PropertyRepositoryService>();
 builder.Services.AddSingleton<IGenericRepositoryService<Guest>, GuestRepositoryService>();
 builder.Services.AddSingleton<IGenericRepositoryService<HouseOwner>, HouseOwnerRepositoryService>();
+builder.Services.AddSingleton<ILoginService, LoginService>();
 
 var app = builder.Build();
 
