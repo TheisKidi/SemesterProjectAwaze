79b285ad95737e689a2b4e52ef691b40f3bab67c
BigHars <hassethore@gmail.com>
2023-05-04 12:04:58 +0200

    LoginService fail

--- SemesterProjectAwaze/Services/LoginService.cs
+++ SemesterProjectAwaze/Services/LoginService.cs
@@ -4,18 +4,17 @@ namespace SemesterProjectAwaze.Services
 {
     public class LoginService : ILoginService
     {
-        private IGenericRepositoryService<Guest> _guestService;
-        private IGenericRepositoryService<HouseOwner> _houseOwnerService;
+        private HouseOwnerRepositoryServiceDB _houseOwnerService;
 
-        public LoginService(IGenericRepositoryService<Guest> guestService, IGenericRepositoryService<HouseOwner> houseOwnerService)
+        public LoginService(HouseOwnerRepositoryServiceDB houseOwnerService)
         {
-            _guestService = guestService;
             _houseOwnerService = houseOwnerService;
         }
         public async Task<string> LoginAsync(string email, string password)
         {
             // chech HouseOwner
-            var houseOwner = await _houseOwnerService.GetAll().FirstOrDefaultAsync()
+            var houseOwner = await _houseOwnerService.GetAll().FirstOrDefaultAsync(ho => ho.Email == email);
+
         }
     }
 }
