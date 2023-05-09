047774831dbfb7fc8e1a4d7fc6c9eacdae81a057
TheisKidi <theis.k@hotmail.com>
2023-05-09 20:51:31 +0200

    Browse Done

---
+++ SemesterProjectAwaze/wwwroot/js/SelectHouseType.js
@@ -0,0 +1,17 @@
+﻿
+
+
+function selectHouseType(element) {
+    if (element.classList.contains("selected")) {
+        element.classList.remove("selected")
+        return
+    }
+
+    element.classList.add("selected")
+
+
+}
+
+
+
+const houseTypeGroup = document.getElementById("selectedIcons")
