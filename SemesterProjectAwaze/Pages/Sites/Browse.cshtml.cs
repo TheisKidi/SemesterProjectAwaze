108627985fd64c950981a85403a8e3285665614f
TheisKidi <theis.k@hotmail.com>
2023-05-11 12:14:18 +0200

    Visual browse. Svg to img

--- SemesterProjectAwaze/Pages/Sites/Browse.cshtml.cs
+++ SemesterProjectAwaze/Pages/Sites/Browse.cshtml.cs
@@ -15,28 +15,22 @@ namespace SemesterProjectAwaze.Pages.Sites
             _propRepo = propRepo;
         }
 
-
-
         [BindProperty]
         public List<Property> Properties { get; set; }
-        [BindProperty]
-        public HashSet<Country> unique { get; set; }
-        [BindProperty]
-        public HashSet<HouseType> uniqueType { get; set; }
 
         public void OnGet()
         {
-
             Properties = _propRepo.GetAll();
-
-            //unique = new HashSet<Country>(Properties.Select(c => c.Country));
-            //uniqueType = new HashSet<HouseType>(Properties.Select(c => c.HouseType));
         }
 
         public IActionResult OnPost()
         {
-
             return RedirectToPage("SelectedProperty");
+        }
+
+        public IActionResult OnPostFilter()
+        {
+            return null;
         }
 
     }
